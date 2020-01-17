d40df9e10f0723390e9b7569f3c426a1855a66bd
Eric Kong <kong44@scholar-fe01.rcac.purdue.edu>
2020-01-17 15:44:31 -0500

    Final submission for assignment 1

--- Lab01/kong44_Exercise_3.3.py
+++ Lab01/kong44_Exercise_3.3.py
@@ -23,6 +23,11 @@ Using function and strings to print the object below:
 + - - - - + - - - - +
 
 """
+plus_line = "+ - - - - + - - - - +"
+pipe_line = "|         |         |"
+plus_line2 = plus_line*2
+pipe_line2 = pipe_line*2
+
 # this function accepts 2 arguments
 def do_once(arg0,val0):
     arg0(val0)
@@ -43,11 +48,43 @@ def print_once(string2):
 
 # printing the shape
 # combines the earlier functions 
-do_once(print_once,"+ - - - - + - - - - +")
-do_twice(print_twice,"|         |         |") 
-do_once(print_once,"+ - - - - + - - - - +")
-do_twice(print_twice,"|         |         |") 
-do_once(print_once,"+ - - - - + - - - - +")
+def single_shape(do_once,do_twice):
+    do_once(print_once, plus_line)
+    do_twice(print_twice,pipe_line) 
+    do_once(print_once,plus_line)
+    do_twice(print_twice,pipe_line) 
+    do_once(print_once,plus_line)
+    
+def grid_shape(do_once,do_twice):
+    do_once(print_once, plus_line2)
+    do_twice(print_twice,pipe_line2) 
+    do_once(print_once,plus_line2)
+    do_twice(print_twice,pipe_line2) 
+    do_once(print_once,plus_line2)
+
+single_shape(do_once,do_twice) #runs the single shape function 
 
 ##################################################################
-print("--------------------Dividing Line--------------")
+print("\n\n--------------------Dividing Line--------------")
+print("---------------------Part 2 Below------------------\n")
+
+grid_shape(do_once,do_twice)
+grid_shape(do_once,do_twice)
+
+##################################################################
+print("\n\n--------------------Dividing Line--------------")
+print("-----------Personal Practice Below, uncomment to see--------\n")
+
+# playing around 
+# using what I learned from Learn Python The Hard Way to prompt the user
+# =============================================================================
+# grid_question = input("Do you want to print a larger grid (Y or N?)\n")
+# 
+# if grid_question == "Y":
+#     grid_shape(do_once,do_twice)
+#     grid_shape(do_once,do_twice)
+#     
+# else: 
+#     quit()
+# =============================================================================
+    
